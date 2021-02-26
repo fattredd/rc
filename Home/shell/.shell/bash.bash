259605a0eec20000ee9c0b83c380502ecc6e28cc
JButler <fattredd@gmail.com>
2021-02-26 16:56:07 +0000

    Added aptlist

--- Home/shell/.shell/bash.bash
+++ Home/shell/.shell/bash.bash
@@ -3,7 +3,7 @@
 # fzf must come after this line, it appears to remove the cd completion
 [ -f ~/.fzf.bash ] && source ~/.fzf.bash && complete -o nospace -o plusdirs -F _fzf_dir_completion cd
 
-[ -f ~/.local/bin/thefuck] && eval "$(thefuck --alias)"
+[ -f ~/.local/bin/thefuck ] && eval "$(thefuck --alias)"
 
 if shopt -q login_shell; then
 	echo Sourced
