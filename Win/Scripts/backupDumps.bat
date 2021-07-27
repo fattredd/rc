a2ebfc2f22b109befde38c0952aa745174e1137b
JButler <fattredd@gmail.com>
2021-07-27 12:24:20 -0400

    Added bsod backups

---
+++ Win/Scripts/backupDumps.bat
@@ -0,0 +1,14 @@
+@echo off
+
+set source=C:\Windows\Minidump\*.dmp
+set dest=\\10.0.0.19\Other\winDump
+
+for /f "tokens=*" %%i in ('hostname') do (
+  set host=%%i
+  goto :done
+  )
+:done
+
+md %dest%\%host%\
+echo Copying Minidump files from %source% to %dest%\%host%\
+copy %source% %dest%\%host%\
