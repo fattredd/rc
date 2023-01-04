81ce65ccf2dca1bb4b4ece0ec4b20d7a0e4fc187
JButler <jbutler@luf.co>
2023-01-04 09:43:41 -0500

    New stuff please

--- Home/bin/bin/conn.sh
+++ Home/bin/bin/conn.sh
@@ -1,20 +1,21 @@
 #!/bin/bash
 
-echo "Connecting to devex container"
+cnt_name=${1:-"dev-env"}
+echo "Connecting to $cnt_name container"
 
 working_dir="$(realpath --relative-base="$HOME" .)"
 echo "Trying to find ~/$working_dir..."
 
-docker exec devex bash -c "[ -d \$(pwd)/$working_dir ]"
+docker exec $cnt_name bash -c "[ -d \$(pwd)/$working_dir ]"
 dirFound=$?
 internal_path=
 if [ $dirFound -eq 0 ]; then
-    internal_path="$(docker exec devex bash -c "echo \$(pwd)/$working_dir")"
+    internal_path="$(docker exec $cnt_name bash -c "echo \$(pwd)/$working_dir")"
     echo "  in dir: ~/$working_dir"
 
 else
-    echo "  Local dir not found in container"
+    echo "  Local dir not found in $cnt_name"
 fi
 
-docker exec -it -w "$internal_path" devex /bin/bash
+docker exec -it -w "$internal_path" $cnt_name /bin/bash
 
