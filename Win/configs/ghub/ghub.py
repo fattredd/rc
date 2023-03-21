e697e83be89286a2b8842fb915e141470003d524
JButler <fattredd@gmail.com>
2023-03-21 14:53:26 -0400

    win - ghub - restart ghub on load/dump

--- Win/configs/ghub/ghub.py
+++ Win/configs/ghub/ghub.py
@@ -1,26 +1,52 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python
 
-# Import/export G Hub settings for version control
+# load/dump G Hub settings for version control
 
-import sqlite3, sys, os, pathlib
+import sqlite3, sys, os, pathlib, subprocess
 
 home = pathlib.Path.home()
-config_file = os.path.join(home, "AppData", "Local", "LGHUB", "settings2.db")
-export_file = os.path.join(os.path.dirname(os.path.realpath(__file__)), "ghub.json")
+config_file = os.path.join(home, "AppData", "Local", "LGHUB", "settings.db")
+dump_file = os.path.join(os.path.dirname(os.path.realpath(__file__)), "ghub.json")
 
-if sys.argv[1] == "export":
+def get_process(proc_name="lghub_agent.exe"):
+  result = subprocess.run(
+    ["wmic", "process", "where", f"name='{proc_name}'", "get", "ExecutablePath"],
+    stdout=subprocess.PIPE)
+  data_str = result.stdout.decode("ASCII")
+  return data_str.strip().split("\r\r\n")[1]
+
+def fork_process(proc_path):
+  subprocess.Popen([proc_path],
+    creationflags=subprocess.CREATE_NEW_PROCESS_GROUP)
+
+def kill_process(proc_name):
+  subprocess.run(["taskkill", "/F", "/IM", proc_name])
+
+if len(sys.argv) == 1:
+  print("Either load or dump")
+  exit(1)
+if sys.argv[1] == "dump":
   config = {}
+  agent_proc = get_process("lghub_agent.exe")
+  kill_process("lghub_agent.exe")
   with sqlite3.connect(config_file) as conf_db:
     cur = conf_db.cursor()
     res = cur.execute("SELECT * FROM main.data")
     config_bytes = res.fetchone()[2]
-    with open(export_file, 'wb') as f:
+    with open(dump_file, 'wb+') as f:
       f.write(config_bytes)
-elif sys.argv[1] == "import":
-  with open(export_file, 'rb') as f:
+  fork_process(agent_proc)
+  print("dumped to", dump_file)
+elif sys.argv[1] == "load":
+  agent_proc = get_process("lghub_agent.exe")
+  kill_process("lghub_agent.exe")
+  with open(dump_file, 'rb') as f:
     config_bytes = f.read()
     with sqlite3.connect(config_file) as conf_db:
       cur = conf_db.cursor()
       res = cur.execute(f"UPDATE data SET file = (?)", (config_bytes,))
+  fork_process(agent_proc)
+  print("loaded from", dump_file)
 else:
-  print("Either import or export")
+  print("Either load or dump")
+  exit(1)
